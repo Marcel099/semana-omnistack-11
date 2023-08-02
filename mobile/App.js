149461d52db071b92975a55a9d4f7d7b569a12a7
Marcelo Lupatini <mar.lupatini@gmail.com>
2023-08-02 17:02:07 -0300

    Upgrade do Expo de v43 para v44

--- mobile/App.js
+++ mobile/App.js
@@ -3,11 +3,14 @@ import 'intl/locale-data/jsonp/pt-BR'
 
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 import Routes from './src/routes'
 
 export default function App() {
   return (
-    <Routes/>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Routes/>
+    </GestureHandlerRootView>
   );
 }
